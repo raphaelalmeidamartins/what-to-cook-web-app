a40bf013ff129ef88bb10344b03c8563117eab5d
Lalá Nametala <lalanametala@gmail.com>
2022-02-11 15:48:25 -0300

    Feature: test file added, and transfer functions to the script.js file

---
+++ tests/fetchRecipes.test.js
@@ -0,0 +1,14 @@
+const { urlGenerator } = require('../src/fetchRecipes');
+
+describe('Test the urlGenerator function', () => {
+	it('is a function', () => {
+		expect(typeof urlGenerator).toBe('function');
+	});
+	it('returns the expected URL', () => {
+		const param = '&diet=${dietType}&health=${healthInfo}&cuisineType=${cuisineType}&mealType=${mealType}&dishType=${dishType}&calories=${caloriesDropdown}';
+
+		const expected = 'https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=7c7cd4bd&app_key=545204c90ab3de54ab8d84cd5aaba9dc&diet=${dietType}&health=${healthInfo}&cuisineType=${cuisineType}&mealType=${mealType}&dishType=${dishType}&calories=${caloriesDropdown}&random=true';
+
+		expect(urlGenerator('chicken', param)).toBe(expected);
+	});
+});
