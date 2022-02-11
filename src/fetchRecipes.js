23e773e55bb67b93a95a1df64871ffc730796dae
Lalá Nametala <lalanametala@gmail.com>
2022-02-11 20:10:12 -0300

    Feature: creation of separate fetch functions for food, drink and search - and fixes on the test file

--- src/fetchRecipes.js
+++ src/fetchRecipes.js
@@ -1,19 +1,41 @@
-const searchElement = localStorage.getItem('searchElement');
-const storedArray = JSON.parse(localStorage.getItem('otherElements'));
+// const storedArray = JSON.parse(localStorage.getItem('otherElements'));
 
-const urlGenerator = (searchElement, storedArray) => {	
-	const url = storedArray.reduce((acc, urlString) => acc + urlString, `https://api.edamam.com/api/recipes/v2?type=public&q=${searchElement}&app_id=7c7cd4bd&app_key=545204c90ab3de54ab8d84cd5aaba9dc`);
+const urlGenerator = (keyword, storedArray) => {	
+	const url = storedArray.reduce((acc, urlString) => acc + urlString, `https://api.edamam.com/api/recipes/v2?type=public&q=${keyword}&app_id=7c7cd4bd&app_key=545204c90ab3de54ab8d84cd5aaba9dc`);
 
 	return `${url}&random=true`;
 };
 
-//função fetch para buscar as receitas na API
-const fetchRecipes = async () => {
+//função fetch para buscar as receitas com palavra-chave na API
+const fetchSearchRecipes = async (storedArray) => {
+	const searchElement = localStorage.getItem('searchElement');
 	const response = await fetch(urlGenerator(searchElement, storedArray));
 	const data = await response.json();
 	return data;
 };
 
-module.exports = { fetchRecipes, urlGenerator };
+
+//função fetch para buscar as receitas de comida na API
+const fetchFoodRecipes = async (storedArray) => {
+	const foodKeywords = ['chicken', 'beef', 'rice', 'beans', 'pasta', 'sauce', 'potato', 'salad', 'fish', 'seafood', 'vegetable', 'tomato', 'chinese', 'italian', 'french', 'pork', 'sausage', 'ham', 'pumpkin', 'egg', 'bread', 'pastry', 'dairy', 'cheese', 'soup', 'dressing', 'cereal', 'meat', 'bakery', 'turkey', 'pepper'];
+	const foodElement = foodKeywords[Math.floor(Math.random()*foodKeywords.length)];
+	const response = await fetch(urlGenerator(foodElement, storedArray));
+	const data = await response.json();
+	return data;
+};
+
+
+//função fetch para buscar as receitas de bebida na API
+const fetchDrinkRecipes = async (storedArray) => {
+	const drinkKeywords = ['water', 'soda', 'vodka', 'gin', 'beer', 'wine', 'rum', 'juice'];
+	const drinkElement = drinkKeywords[Math.floor(Math.random()*drinkKeywords.length)];
+	const array = storedArray;
+	array.push('&dishType=Drink');
+	const response = await fetch(urlGenerator(drinkElement, array));
+	const data = await response.json();
+	return data;
+};
+
+module.exports = { fetchSearchRecipes, fetchFoodRecipes, fetchDrinkRecipes, urlGenerator };
 
 //chaves de recipe que serão usadas: ['label', 'image', 'images'(obj), 'source', 'url', 'yield'(quantas porções rende - numero), 'dietLabels'(arr), 'healthLabels'(arr), 'cautions'(arr), 'ingredientLines'(arr - cada elemento é uma linha da receita), 'calories' (calorias totais de todas as porções), 'totalWeight', 'totalTime (tempo de preparo em minutos)', 'cuisineType' (arr), 'mealType' (arr), 'dishType' (arr), 'totalNutrients'(obj de obj - {label: 'Energy', quantity: 1617.7713676499998, unit: 'kcal'} - CHAVES QUE SERÃO USADAS: [ENERC_KCAL, FAT, CHOCDF, PROCNT, CHOLE, NA]), 'totalDaily'(obj - MESMAS OBSERVAÇÕES QUE O totalNutrients)]
--- tests/fetchRecipes.test.js
+++ tests/fetchRecipes.test.js
@@ -5,9 +5,9 @@ describe('Test the urlGenerator function', () => {
 		expect(typeof urlGenerator).toBe('function');
 	});
 	it('returns the expected URL', () => {
-		const param = '&diet=${dietType}&health=${healthInfo}&cuisineType=${cuisineType}&mealType=${mealType}&dishType=${dishType}&calories=${caloriesDropdown}';
+		const param = ['&diet=${dietType}', '&calories=${caloriesDropdown}'];
 
-		const expected = 'https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=7c7cd4bd&app_key=545204c90ab3de54ab8d84cd5aaba9dc&diet=${dietType}&health=${healthInfo}&cuisineType=${cuisineType}&mealType=${mealType}&dishType=${dishType}&calories=${caloriesDropdown}&random=true';
+		const expected = 'https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=7c7cd4bd&app_key=545204c90ab3de54ab8d84cd5aaba9dc&diet=${dietType}&calories=${caloriesDropdown}&random=true';
 
 		expect(urlGenerator('chicken', param)).toBe(expected);
 	});
