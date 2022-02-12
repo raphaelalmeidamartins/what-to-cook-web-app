839a7caf07ba1f5ba75aa94bb0a35da84c5ec8f1
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-02-12 16:19:47 -0300

    Style
    Forms/chat bubble-animation implementation in progress.

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+// icons selectors
+
 const searchIcon = document.querySelector('#search');
 const foodIcon = document.querySelector('#food');
 const drinkIcon = document.querySelector('#drink');
 const refreshIcon = document.querySelector('#refresh');
 
+// loading screen selector
+
 const loadingScreen = document.querySelector('#loading');
+
+// tabs selectors
 
 const header = document.querySelector('#header');
 const navbar = document.querySelector('#navbar');
@@ -11,6 +17,10 @@ const allTabs = document.querySelector('#main-container');
 const tabFood = document.querySelector('#recipe-container');
 const tabDrink = document.querySelector('#drink-container');
 const tabSearch = document.querySelector('#search-container');
+
+// form selectors
+
+const form = document.querySelector('#preferences-form');
 
 const changeTab = (element, button) => {
 	const previousButton = document.querySelector('.selected');
@@ -43,9 +53,38 @@ foodIcon.addEventListener('click', () => changeTab(tabFood, foodIcon));
 drinkIcon.addEventListener('click', () => changeTab(tabDrink, drinkIcon));
 searchIcon.addEventListener('click', () => changeTab(tabSearch, searchIcon));
 
+const displayElement = (element, timer) => {
+	element.classList.remove('hidden');
+	setTimeout(() => 	element.classList.add('display'), timer);
+};
+
+const hiddenElement = (element, timer) => {
+	element.classList.remove('display');
+	setTimeout(() => 	element.classList.add('hidden'), timer);
+};
+
+const conversationFlow = () => {
+	const hiddenElementsArray = document.querySelectorAll('.hidden');
+	let stop = true;
+	while (stop) {
+		hiddenElementsArray.forEach((currentElement, index) => {
+			if (currentElement.tagName === 'DIV') {
+				stop = false;
+				displayElement(currentElement, 1000 * [index + 1]);
+				throw console.log('Parou');
+			} else {
+				displayElement(currentElement, 1000 * [index + 1]);
+			}
+		});
+	}
+};
+
+// https://web.dev/learn/css/transitions/
+
 window.onload = () => {
-	startLoading();
-	setTimeout(() => {
-		stopLoading();
-	}, 1000);
+	// startLoading();
+	// setTimeout(() => {
+	// 	stopLoading();
+	// }, 1000);
+	conversationFlow();
 };
