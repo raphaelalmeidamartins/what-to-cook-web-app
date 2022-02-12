5621f4f3ebcd529347e36d1f8985dca4d0c0a158
Wendryo S <wendryo.sales@gmail.com>
2022-02-11 22:25:24 -0300

    Feature
    implementing button logics - in progress...

---
+++ script.js
@@ -0,0 +1,25 @@
+const searchIcon = document.querySelector('#search');
+const foodIcon = document.querySelector('#food');
+const drinkIcon = document.querySelector('#drink');
+const refreshIcon = document.querySelector('#refresh');
+
+const tabFood = document.querySelector('#recipe-container');
+const tabDrink = document.querySelector('#drink-container');
+const tabSearch = document.querySelector('#search-container');
+
+const changeTo = (element) => {
+	const previous = document.querySelector('.visible');
+	if (previous) {
+	  previous.classList.remove('visible');
+	  previous.classList.add('invisible');
+	}
+	element.classList.remove('invisible');
+	element.classList.add('visible');
+}
+
+foodIcon.addEventListener('click', () => changeTo(tabFood));
+searchIcon.addEventListener('click', () => changeTo(tabSearch));
+drinkIcon.addEventListener('click', () => changeTo(tabDrink));
+
+
+window.onload = () => {};
